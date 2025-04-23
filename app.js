9101d5da8ee5f5f1a6db2723ea420849b0864f99
arthursnt <santanaarthur090508@gmail.com>
2025-04-22 21:32:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let nome = 'Arthur';
+console.log(nome)
+console.log('Vai Corinthians');
+console.log((10 > 5) && (5 > 1));
+console.log((4 == 4) && (10 < 5));
+console.log((3 >= 2) && (2 <= 2));
+console.log((7 != 7) && (5 > 4));
+console.log((8 > 3) && (3 < 10));
